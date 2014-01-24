6edf144bd55aeff8ffb07aaa4d9ccfe93b642948
Mischback <dev@mischback.de>
2014-01-24 18:38:36 +0100

    Create login form

--- oweb/urls.py
+++ oweb/urls.py
@@ -1,10 +1,13 @@
 # Django imports
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('oweb.views', 
-    url(r'^$', 'home'),
+urlpatterns = patterns('oweb.views',
+    url(r'^$', 'home', name='home'),
 )
 
 urlpatterns += patterns('',
-    url(r'^login/$', 'django.contrib.auth.views.login', name='app_login'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {'template_name': 'app_login.html'},
+        name='app_login'),
 )
