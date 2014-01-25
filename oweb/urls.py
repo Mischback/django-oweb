c4d78c913e6f92c3bbeb30cc836e708bdb4063bf
Mischback <dev@mischback.de>
2014-01-25 15:28:05 +0100

    Working on planet views

--- oweb/urls.py
+++ oweb/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('oweb.views',
     url(r'^$', 'home', name='home'),
     url(r'^(?P<account_id>\d+)$', 'account_overview', name='account_overview'),
+
+    url(r'^planet/(?P<planet_id>\d+)$', 'planet_overview', name='planet_overview'),
+    url(r'^(?P<account_id>\d+)/planet-create$', 'planet_create', name='planet_create'),
 )
 
 urlpatterns += patterns('',
---
+++ oweb/views/planet.py
@@ -0,0 +1,70 @@
+# Python imports
+from datetime import datetime
+import hashlib
+# Django imports
+from django.core.urlresolvers import reverse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
+# app imports
+from oweb.models import Account, Building, Planet, Research113, Research122
+
+
+def planet_overview(req, planet_id):
+    """
+    """
+    # this is the non-decorator version of the login_required decorator
+    # basically it checks, if the user is authenticated and redirects him, if
+    # not. The decorator could not handle the reverse url-resolution.
+    if not req.user.is_authenticated():
+        return redirect(reverse('oweb:app_login'))
+
+    # fetch the account and the list of planets
+    try:
+        planet = Planet.objects.select_related('account').get(id=planet_id)
+    except Planet.DoesNotExist:
+        raise Http404
+
+    # checks, if this account belongs to the authenticated user
+    if not req.user.id == planet.account.owner_id:
+        raise Http404
+
+    planets = Planet.objects.filter(account_id=planet.account.id)
+    plasma = get_object_or_404(Research122, account=planet.account.id)
+    energy = get_object_or_404(Research113, account=planet.account.id)
+    buildings = get_list_or_404(Building, planet=planet_id)
+
+    planet_fields = 0
+    for b in buildings:
+        planet_fields += b.level
+
+    return render(req, 'oweb_planet_overview.html',
+        {
+            'account': planet.account,
+            'planet': planet,
+            'planet_fields': planet_fields,
+            'planets': planets,
+        }
+    )
+
+
+def planet_create(req, account_id):
+    # this is the non-decorator version of the login_required decorator
+    # basically it checks, if the user is authenticated and redirects him, if
+    # not. The decorator could not handle the reverse url-resolution.
+    if not req.user.is_authenticated():
+        return redirect(reverse('oweb:app_login'))
+
+    account = get_object_or_404(Account, pk=account_id)
+
+    # checks, if this account belongs to the authenticated user
+    if not req.user.id == account.owner_id:
+        raise Http404
+
+    tmp_name = hashlib.md5(str(datetime.now))
+    Planet.objects.create(account=account, name=tmp_name)
+
+    planet = get_object_or_404(Planet, name=tmp_name)
+    planet.name = 'Colony'
+    planet.save()
+
+    return HttpResponseRedirect(reverse('oweb:planet_overview', args=(planet.id,)))
