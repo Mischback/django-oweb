7b74eb7b56bfdbebd0fdc99f03e1a45bbe26823c
Mischback <dev@mischback.de>
2014-02-01 23:03:35 +0100

    daily_production

--- oweb/templatetags/oweb_tags.py
+++ oweb/templatetags/oweb_tags.py
@@ -9,3 +9,9 @@ def number_beautify(value):
     """
     """
     return number_format(int(value), force_grouping=True)
+
+@register.filter
+def daily_production(value):
+    """
+    """
+    return value * 24
