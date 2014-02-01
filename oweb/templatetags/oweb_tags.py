1ab3bb1267835decac49b3882937ea3be4fc7c31
Mischback <dev@mischback.de>
2014-02-01 22:55:54 +0100

    number_beautify
    Insert 1k-seperators into numbers

---
+++ oweb/templatetags/oweb_tags.py
@@ -0,0 +1,11 @@
+# Django imports
+from django import template
+from django.utils.formats import number_format
+
+register = template.Library()
+
+@register.filter
+def number_beautify(value):
+    """
+    """
+    return number_format(int(value), force_grouping=True)
