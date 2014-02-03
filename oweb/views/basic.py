1fc23593c4be1b1123e16d78116bb2538187b06a
Mischback <dev@mischback.de>
2014-02-03 20:43:58 +0100

    Working on issue https://github.com/Mischback/django-oweb/issues/24

---
+++ oweb/views/basic.py
@@ -0,0 +1,23 @@
+# Django imports
+from django.shortcuts import get_list_or_404, render
+# app imports
+from oweb.models import Account
+
+def home(req):
+    """
+    @brief  Overview of all accounts of a user
+    """
+    # this is the non-decorator version of the login_required decorator
+    # basically it checks, if the user is authenticated and redirects him, if
+    # not. The decorator could not handle the reverse url-resolution.
+    if not req.user.is_authenticated():
+        return redirect(reverse('oweb:app_login'))
+
+    # get all accounts of this user
+    accounts = get_list_or_404(Account, owner_id=req.user.id)
+    # render the template
+    return render(req, 'oweb/home.html', 
+        {'accounts': accounts}
+    )
+
+
