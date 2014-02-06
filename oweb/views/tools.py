5525e35be022f4bba4d125b5a5003cc56d8a1d1c
Mischback <dev@mischback.de>
2014-02-06 21:17:20 +0100

    Working on issue https://github.com/Mischback/django-oweb/issues/7
    Created necessary files

---
+++ oweb/views/tools.py
@@ -0,0 +1,7 @@
+"""Provides some general, account-related tools"""
+# Django imports
+from django.http import Http404
+
+def tools_energy(req, account_id):
+    """Shows some energy related information"""
+    raise Http404
