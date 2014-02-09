f6a84ecd588d76cca9041890f80cfcc44d159f7d
Mischback <dev@mischback.de>
2014-02-09 21:18:09 +0100

    include defense points

--- oweb/libs/points.py
+++ oweb/libs/points.py
@@ -2,13 +2,16 @@
 from django.shortcuts import get_list_or_404
 # app imports
 from oweb.models.building import Building, Supply1, Supply2, Supply3, Supply4, Supply12
+from oweb.models.defense import Defense
 
 def get_planet_points(planet_id):
     buildings = get_list_or_404(Building, planet=planet_id)
+    defense = get_list_or_404(Defense, planet=planet_id)
 
     building_points = 0
     other_points = 0
     production_points = 0
+    defense_points = 0
 
     for b in buildings:
         this_building_cost = b.get_total_cost()
@@ -21,6 +24,12 @@ def get_planet_points(planet_id):
 
         building_points += this_building_points
 
-    planet_points = building_points
+    for d in defense:
+        this_defense = d.as_real_class()
+        this_defense_points = this_defense.count * (this_defense.cost[0] + this_defense.cost[1] + this_defense.cost[2])
 
-    return (planet_points, building_points, production_points, other_points)
+        defense_points += this_defense_points
+
+    planet_points = building_points + defense_points
+
+    return (planet_points, building_points, production_points, other_points, defense_points)
