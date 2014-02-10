d427d3cc5e83eef6410a00dde7f9eccb4f29b482
Mischback <dev@mischback.de>
2014-02-10 08:04:14 +0100

    Do not include buildings
    Production and other should be sufficient

--- oweb/libs/points.py
+++ oweb/libs/points.py
@@ -8,7 +8,6 @@ def get_planet_points(planet_id):
     buildings = get_list_or_404(Building, planet=planet_id)
     defense = get_list_or_404(Defense, planet=planet_id)
 
-    building_points = 0
     other_points = 0
     production_points = 0
     defense_points = 0
@@ -22,14 +21,12 @@ def get_planet_points(planet_id):
         else:
             other_points += this_building_points
 
-        building_points += this_building_points
-
     for d in defense:
         this_defense = d.as_real_class()
         this_defense_points = this_defense.count * (this_defense.cost[0] + this_defense.cost[1] + this_defense.cost[2])
 
         defense_points += this_defense_points
 
-    planet_points = building_points + defense_points
+    planet_points = production_points + other_points + defense_points
 
-    return (planet_points, building_points, production_points, other_points, defense_points)
+    return (planet_points, production_points, other_points, defense_points)
