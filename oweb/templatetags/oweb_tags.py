dcbd2cab76e1236b7d263542623a7e08031c05b0
Mischback <dev@mischback.de>
2014-02-16 17:10:07 +0100

    Document templatetags

--- oweb/templatetags/oweb_tags.py
+++ oweb/templatetags/oweb_tags.py
@@ -1,3 +1,4 @@
+"""These tags are used to enhance the templates"""
 # Django imports
 from django import template
 from django.utils.formats import number_format
@@ -6,12 +7,10 @@ register = template.Library()
 
 @register.filter
 def number_beautify(value):
-    """
-    """
+    """Inserts seperators into large numbers"""
     return number_format(int(value), force_grouping=True)
 
 @register.filter
 def daily_production(value):
-    """
-    """
+    """Calculates the production in 24 hours"""
     return value * 24
