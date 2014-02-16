d6342ad6ad76606db1524be145734bb12c3893ea
Mischback <dev@mischback.de>
2014-02-16 21:10:56 +0100

    Redirect home -> login

--- oweb/tests/views.py
+++ oweb/tests/views.py
@@ -1,6 +1,11 @@
+# Django imports
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 class OWebViewTests(TestCase):
+    """Provides view related tests"""
 
-    def test_home(self):
-        self.assertEqual(True, False)
+    def test_home2login(self):
+        """Redirect from home to login for unauthenticated users"""
+        r = self.client.get(reverse('oweb:home'))
+        self.assertRedirects(r, reverse('oweb:app_login'), status_code=302, target_status_code=200)
