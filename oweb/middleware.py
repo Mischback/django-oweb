957a0152b49d278ff59ba304403a80a586556912
Mischback <dev@mischback.de>
2014-02-16 15:23:00 +0100

    First version of the middleware with custom 404.html template

--- oweb/middleware.py
+++ oweb/middleware.py
@@ -1,9 +1,16 @@
 """App specific middleware"""
-
+# Django imports
+from django.views.defaults import page_not_found
+from django.http import HttpResponseNotFound
+# app imports
 from oweb.exceptions import OWebException
 
 class OWebExceptionMiddleware(object):
+    """Catches OWeb specific Exceptions"""
     def process_exception(self, req, e):
         if isinstance(e, OWebException):
-            raise RuntimeError('custom exception found')
+            return HttpResponseNotFound(
+                page_not_found(req,
+                    template_name='oweb/404.html')
+            )
         return None
