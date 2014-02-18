487a7e75510d24113200137b3a96a2b3925b037d
Mischback <dev@mischback.de>
2014-02-18 21:15:36 +0100

    Stubs for oweb.views.updates.planet_settings_commit

--- oweb/tests/bak.py
+++ oweb/tests/bak.py
@@ -11,10 +11,6 @@ class OWebViewLoginRequiredTests(OWebViewTests):
     this implementations are provided.
     """
 
-    def test_planet_settings_commit(self):
-        r = self.client.get(reverse('oweb:planet_settings_update', args=[1,]))
-        self.assertRedirects(r, reverse('oweb:app_login'), status_code=302, target_status_code=200)
-
     def test_planet_create(self):
         r = self.client.get(reverse('oweb:planet_create', args=[1,]))
         self.assertRedirects(r, reverse('oweb:app_login'), status_code=302, target_status_code=200)
@@ -45,13 +41,6 @@ class OWebViewAccountOwnerTests(OWebViewTests):
     def setUp(self):
         # prepare a client login
         self.client.login(username='test02', password='foo')
-
-    def test_planet_settings_commit(self):
-        """Can somebody update a planet he doesn't posess?"""
-        # no need to perform a real POST request here, since the check is
-        # performed before actual POST-parameters are considered
-        r = self.client.get(reverse('oweb:planet_settings_update', args=[1,]))
-        self.assertEqual(r.status_code, 404)
 
     def test_planet_create(self):
         """Can somebody create a planet for an account he doesn't posess?"""
@@ -104,21 +93,6 @@ class OWebViewAccountOwnerTests(OWebViewTests):
 
 class OWebViewUpdatesTests(OWebViewTests):
     """Tests for views in views/updates.py"""
-
-    def test_planet_settings_commit_no_post(self):
-        """What does ``planet_settings_commit()`` do, if no POST data is provided?"""
-        # TODO insert real test here (should raise OWebDoesNotExist)
-        self.assertEqual(True, True)
-
-    def test_planet_settings_commit_post_tamper(self):
-        """What does happen, if somebody tampers POST data?"""
-        # TODO insert real test here
-        self.assertEqual(True, True)
-
-    def test_planet_settings_commit_redirect(self):
-        """Does ``planet_settings_commit()`` redirect to the correct page?"""
-        # TODO insert real test here (should redirect to planet_settings)
-        self.assertEqual(True, True)
 
     def test_planet_create_redirect(self):
         """Does ``planet_create()`` redirect to the correct page?"""
