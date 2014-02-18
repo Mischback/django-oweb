ecd1740606da935a13b9ad60758ec1e764d01017
Mischback <dev@mischback.de>
2014-02-18 18:21:15 +0100

    This is really work in progress
    Let's modify the middleware, so that it returns real HTTP answers, but not 404 on all occasions

--- oweb/middleware.py
+++ oweb/middleware.py
@@ -1,14 +1,19 @@
 """App specific middleware"""
 # Django imports
 from django.views.defaults import page_not_found
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.template import loader, Context
 # app imports
-from oweb.exceptions import OWebException
+from oweb.exceptions import OWebException, OWebSecurityIncident
 
 class OWebExceptionMiddleware(object):
     """Catches OWeb specific Exceptions"""
     def process_exception(self, req, e):
         if isinstance(e, OWebException):
+            if isinstance(e, OWebSecurityIncident):
+                t = loader.get_template('oweb/404.html')
+                c = Context({'foo': 'bar'})
+                return HttpResponseServerError(t.render(c))
             return HttpResponseNotFound(
                 page_not_found(req,
                     template_name='oweb/404.html')
