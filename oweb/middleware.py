cb3694aac70d8e8deb27c343bc2666b4daecf24b
Mischback <dev@mischback.de>
2014-02-19 21:32:05 +0100

    Creating 403 handling (unauthorized account access)

--- oweb/middleware.py
+++ oweb/middleware.py
@@ -4,16 +4,21 @@ from django.views.defaults import page_not_found
 from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.template import loader, Context
 # app imports
-from oweb.exceptions import OWebException, OWebSecurityIncident
+from oweb.exceptions import OWebException, OWebAccountAccessViolation
 
 class OWebExceptionMiddleware(object):
     """Catches OWeb specific Exceptions"""
     def process_exception(self, req, e):
+        # found one of this apps exceptions
         if isinstance(e, OWebException):
-            if isinstance(e, OWebSecurityIncident):
-                t = loader.get_template('oweb/404.html')
-                c = Context({'foo': 'bar'})
-                return HttpResponseServerError(t.render(c))
+
+            # unauthorized access to an account
+            if isinstance(e, OWebAccountAccessViolation):
+                t = loader.get_template('oweb/403.html')
+                c = Context({'request': req})
+                return HttpResponseForbidden(t.render(c))
+
+            # handle with a 404
             return HttpResponseNotFound(
                 page_not_found(req,
                     template_name='oweb/404.html')
