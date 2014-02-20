0c59952338d9ec770e80acfe06c3460ad48f60f5
Mischback <dev@mischback.de>
2014-02-20 20:55:52 +0100

    test_account_owner

--- oweb/tests/views/account_settings_commit.py
+++ oweb/tests/views/account_settings_commit.py
@@ -17,11 +17,14 @@ class OWebViewsAccountSettingsCommitTests(OWebViewTests):
                              status_code=302,
                              target_status_code=200)
 
-    @skip('not yet implemented')
     def test_account_owner(self):
         """Can somebody update an account he doesn't posess?"""
-        # TODO insert real test here (should raise OWebAccountAccessViolation)
-        self.assertEqual(True, True)
+        self.client.login(username='test02', password='foo')
+        # it is not required to test get and post, because both are handled in
+        # the same way (regarding this scenario)
+        r = self.client.get(reverse('oweb:account_settings_commit', args=[1,]))
+        self.assertEqual(r.status_code, 403)
+        self.assertTemplateUsed(r, 'oweb/403.html')
 
     @skip('not yet implemented')
     def test_no_post(self):
