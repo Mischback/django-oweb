e322bb3cf69474ad0109ff123def3a97f8231f74
Mischback <dev@mischback.de>
2014-02-22 05:19:33 +0100

    Improved create_account tests

--- oweb/tests/views/create_account.py
+++ oweb/tests/views/create_account.py
@@ -5,6 +5,7 @@ from unittest import skip
 from django.core.urlresolvers import reverse
 # app imports
 from oweb.tests import OWebViewTests
+from oweb.models.account import Account
 
 
 class OWebViewsCreateAccountTests(OWebViewTests):
@@ -19,6 +20,11 @@ class OWebViewsCreateAccountTests(OWebViewTests):
 
     def test_redirect(self):
         """Does ``create_account()`` redirect to the correct page?"""
+        accs = Account.objects.all()
+        num_accs = len(accs) + 1
         self.client.login(username='test01', password='foo')
         r = self.client.get(reverse('oweb:create_account'))
-        self.assertEqual(r.status_code, 302)
+        self.assertRedirects(r,
+                             reverse('oweb:account_settings', args=[num_accs]),
+                             status_code=302,
+                             target_status_code=200)
